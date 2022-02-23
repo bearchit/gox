88889128048ca3686c8f7ccf9662f74097bc9e31
bearchit <bearchit@gmail.com>
2022-02-23 16:10:58 +0900

    add `WithAt` option on `Available` query

--- entx/available/available.go
+++ entx/available/available.go
@@ -1,7 +1,10 @@
 package available
 
+import "time"
+
 type QueryOption struct {
 	Preview bool
+	At      time.Time
 }
 
 type QueryOptionFunc func(*QueryOption)
@@ -9,5 +12,11 @@ type QueryOptionFunc func(*QueryOption)
 func WithPreview(preview bool) QueryOptionFunc {
 	return func(qo *QueryOption) {
 		qo.Preview = preview
+	}
+}
+
+func WithAt(at time.Time) QueryOptionFunc {
+	return func(qo *QueryOption) {
+		qo.At = at
 	}
 }
